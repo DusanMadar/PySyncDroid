25d64330178699f659abccd4e1207a0a9c03bca3
Dusan Madar <madar.dusan@gmail.com>
2016-02-27 19:18:47 +0000

    FIND DEVICE - make the exception message more helpfull: show a list of devices (if any) found for a given vendor

--- pysyncdroid/find_device.py
+++ pysyncdroid/find_device.py
@@ -10,6 +10,9 @@ from pysyncdroid.exceptions import DeviceException
 
 #:
 # pattern to find MTP connected devices
+# u - user ID
+# b - USB bus ID
+# d - device ID
 MTP_PATH_PATTERN = '/run/user/{u}/gvfs/mtp:host=%5Busb%3A{b}%2C{d}%5D'
 
 
@@ -25,6 +28,8 @@ def connection_details(vendor, model):
     :returns tuple
 
     """
+    vendor_devices = []
+
     vendor_pattern = re.compile(vendor, re.IGNORECASE)
     model_pattern = re.compile(model, re.IGNORECASE)
 
@@ -34,17 +39,32 @@ def connection_details(vendor, model):
     for device_info in usb_devices.split('\n'):
         if vendor_pattern.search(device_info) is None:
             continue
+        else:
+            # collect only the human readable device info (i.e. ignore IDs)
+            vendor_devices.append(device_info[33:])
 
         if model_pattern.search(device_info) is None:
             continue
 
+        # yep, USB bus ID and device ID are between these indexes
         usb_bus = device_info[4:7]
         device = device_info[15:18]
 
         return usb_bus, device
 
     else:
-        exc_msg = 'Device "{v} {m}" not found'.format(v=vendor, m=model)
+        # exception message base
+        exc_msg = 'Device "{v} {m}" not found.\n'.format(v=vendor, m=model)
+
+        # exception message extension base
+        ext_base = '"{v}" devices were found'.format(v=vendor)
+
+        if vendor_devices:
+            exc_msg += ('Following {b}:\n{d}'
+                        .format(b=ext_base, d='\n'.join(vendor_devices)))
+        else:
+            exc_msg += 'No {b}.'.format(b=ext_base)
+
         raise DeviceException(exc_msg)
 
 
@@ -52,9 +72,9 @@ def get_mtp_path(usb_bus, device):
     """
     Get path to the device connected via MTP.
 
-    :argument usb_bus: USB bus number
+    :argument usb_bus: USB bus ID
     :type usb_bus: str
-    :argument device: device number
+    :argument device: device ID
     :type interface: str
 
     :returns str
